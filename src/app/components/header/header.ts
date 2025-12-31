926e355e6330b70061ba7ae26b7aed62034d6c93
VacheronPradelAlyssa <vacheron.alyssa@aol.com>
2025-12-31 14:11:03 +0100

    feat: header adaptatif avec fond accueil + opacité matrix réduite

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.scss',
+  host: {
+    '[class.accueil-theme]': 'isAccueilPage()'
+  }
 })
 export class Header {
+  private router = inject(Router);
+  isAccueilPage = signal(false);
 
+  constructor() {
+    this.checkRoute(this.router.url);
+    
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.checkRoute(event.urlAfterRedirects);
+      });
+  }
+
+  private checkRoute(url: string): void {
+    this.isAccueilPage.set(url === '/' || url === '');
+  }
 }
